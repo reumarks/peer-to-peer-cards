23c557abb6bb544995cba67e60e526713ddde5a7
Reu Zuidema <rzuidema@uwaterloo.ca>
2026-08-16 23:32:02 -0400

    Split card logic into its own module

---
+++ src/scripts/cards.js
@@ -0,0 +1,74 @@
+const cardParent = document.body;
+
+
+let topIndex = 10;
+
+const cardValues = ["ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king"];
+const cardSuits = ["clubs", "diamonds", "hearts", "spades"];
+
+export function createDeck(x, y){
+  let currentIndex = 0;
+  for(let suitIndex = 0; suitIndex < cardSuits.length; suitIndex++){
+    for(let valueIndex = 0; valueIndex < cardValues.length; valueIndex++){
+      createCard(
+        `${cardValues[valueIndex]}-of-${cardSuits[suitIndex]}`, 
+        x + currentIndex * 15, 
+        y
+      );
+      currentIndex++;
+    }
+  }
+}
+
+export function createCard(cardName, x, y){
+  let newCard = document.createElement('img');
+  newCard.className = 'card';
+  newCard.src = `/src/resources/playing-cards/${cardName}.svg`;
+  newCard.alt = cardName;
+  newCard.style.left = x + "px";
+  newCard.style.top = y + "px";
+  newCard.style.zIndex = topIndex + 1;
+  topIndex ++;
+  makeElementDraggable(newCard);
+  cardParent.appendChild(newCard);
+}
+
+function makeElementDraggable(elmnt) {
+  let pPosX = 0, pPosY = 0, posX = 0, posY = 0;
+  elmnt.onmousedown = dragMouseDown;
+
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    pPosX = e.clientX;
+    pPosY = e.clientY;
+
+    // Move card to top
+    elmnt.style.zIndex = topIndex + 1;
+    topIndex++;
+    
+    // Setup stop drag function
+    document.onmouseup = closeDragElement;
+    // call a function whenever the cursor moves
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // calculate the new cursor position:
+    posX = pPosX - e.clientX;
+    posY = pPosY - e.clientY;
+    pPosX = e.clientX;
+    pPosY = e.clientY;
+    // set the element's new position:
+    elmnt.style.top = (elmnt.offsetTop - posY) + "px";
+    elmnt.style.left = (elmnt.offsetLeft - posX) + "px";
+  }
+
+  function closeDragElement() {
+    /* stop moving when mouse button is released:*/
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+}
--- src/scripts/game.js
+++ src/scripts/game.js
@@ -1,10 +1,8 @@
-const gameBoard = document.getElementById('game-container');
-const debugText = document.getElementById('debug-text')
+import { createDeck } from "./cards.js";
+
 let room = null;
 
 let topIndex = 10;
-
-//window.addEventListener('resize', resizeCanvas);
 
 export async function initGame(currentRoom){
   room = currentRoom;
@@ -14,54 +12,9 @@ export async function initGame(currentRoom){
     //if (type === 'mouse-click-event') makeDot(payload);
   });
 
-  // Register all cards as draggable elements
-  let cards = document.getElementsByClassName("card");
-  for(let cardIndex = 0; cardIndex < cards.length; cardIndex++){
-    dragElement(cards[cardIndex]);
-  }
+  createDeck(20, 20, 10);
 
   draw();
-}
-
-// drag element loosely based on https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_draggable
-function dragElement(elmnt) {
-  var pPosX = 0, pPosY = 0, posX = 0, posY = 0;
-  elmnt.onmousedown = dragMouseDown;
-
-  function dragMouseDown(e) {
-    e = e || window.event;
-    e.preventDefault();
-    pPosX = e.clientX;
-    pPosY = e.clientY;
-
-    // Move card to top
-    elmnt.style.zIndex = topIndex + 1;
-    topIndex++;
-    
-    // Setup stop drag function
-    document.onmouseup = closeDragElement;
-    // call a function whenever the cursor moves
-    document.onmousemove = elementDrag;
-  }
-
-  function elementDrag(e) {
-    e = e || window.event;
-    e.preventDefault();
-    // calculate the new cursor position:
-    posX = pPosX - e.clientX;
-    posY = pPosY - e.clientY;
-    pPosX = e.clientX;
-    pPosY = e.clientY;
-    // set the element's new position:
-    elmnt.style.top = (elmnt.offsetTop - posY) + "px";
-    elmnt.style.left = (elmnt.offsetLeft - posX) + "px";
-  }
-
-  function closeDragElement() {
-    /* stop moving when mouse button is released:*/
-    document.onmouseup = null;
-    document.onmousemove = null;
-  }
 }
 
 function draw(){
